35fd72a7c5ccc8948e19eb89dae50f766eb26f5e
nuhu78 <22-48664-3@student.aiub.edu>
2026-07-11 19:17:21 +0600

    startup.sh

--- e_shop/startup.sh
+++ e_shop/startup.sh
@@ -5,4 +5,6 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell
+
 gunicorn e_shop.wsgi --workers 2 --timeout 120
