7ba963d2563b00a2b285f6f9fbe1e4621be3481b
nuhu78 <22-48664-3@student.aiub.edu>
2026-07-12 13:06:14 +0600

    fix: update URL patterns for media serving based on DEBUG setting and change start command in render.yaml

--- e_shop/e_shop/urls.py
+++ e_shop/e_shop/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('accounts/', include('allauth.urls')),
     path('', include('shop.urls')),
 ]
-urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
